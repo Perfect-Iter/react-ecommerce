aa62982a10c3108615ef864d91dfa2900ee8aa62
ruben <barazaruben.br@gmail.com>
2023-09-04 15:22:09 +0300

    todays work

---
+++ src/components/menu-items/MenuItem.tsx
@@ -0,0 +1,24 @@
+
+import './menu-item.styles.scss'
+
+type MenuItemProps = {
+    title: string;
+    imageUrl: string;
+    size?: string;
+}
+
+const MenuItem: React.FC<MenuItemProps> = ({title, imageUrl, size}) => {
+  return (
+    <div className={`${size} menu-item`}  >
+
+        <div className="background-image" style={{backgroundImage: `url(${imageUrl})`}} />
+
+        <div className="content">
+            <h1 className="title">{title.toUpperCase()}</h1>
+            <span className="subtitle">SHOP NOW</span>
+        </div>
+    </div>
+  )
+}
+
+export default MenuItem
